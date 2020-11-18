e9da0b2b9d7d66311a0dba5da8ed60c2d1721592
Carson James <carsonaj@stat.tamu.edu>
2020-11-17 19:43:29 -0600

    added boilerplate

---
+++ src/mdp.cpp
@@ -0,0 +1,8 @@
+#include "RcppArmadillo.h"
+
+// via the depends attribute we tell Rcpp to create hooks for
+// RcppArmadillo so that the build process will know what to do
+//
+// [[Rcpp::depends(RcppArmadillo)]]
+
+// function declarations:
