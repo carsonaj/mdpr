62a7b0f43d15cdf49de3727100ad8038c77e0fda
Carson James <carsonaj@stat.tamu.edu>
2020-11-19 19:19:30 -0600

    deleted

--- src/mdp.cpp
+++
@@ -1,8 +0,0 @@
-#include "RcppArmadillo.h"
-
-// via the depends attribute we tell Rcpp to create hooks for
-// RcppArmadillo so that the build process will know what to do
-//
-// [[Rcpp::depends(RcppArmadillo)]]
-
-// function declarations:
